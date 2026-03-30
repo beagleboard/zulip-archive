da096a111c4267480093323bb35bf639cfceada0
Jason Kridner <jkridner@beagleboard.org>
2026-03-30 15:40:47 -0400

    stabbing in the dark

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,10 +102,6 @@ python3 archive.py -b
 
 cd ${checked_out_repo_path}
 
-git checkout $archive_branch
-
-git fetch origin
-
 current_sha="$(git rev-parse origin/${archive_branch})"
 
 if [[ "$current_sha" != "$initial_sha" ]]
